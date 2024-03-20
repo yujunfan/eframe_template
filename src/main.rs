124ff32dec7c3988f023923984e603510b1b10cf
余俊帆 <seeryujunfan@qq.com>
2024-03-20 13:41:32 +0800

    修改为可以用于本地开发的版本

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,9 @@ fn main() -> eframe::Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "web rt",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(web_rt::TemplateApp::new(cc))),
     )
 }
 
@@ -37,7 +37,7 @@ fn main() {
             .start(
                 "the_canvas_id", // hardcode it
                 web_options,
-                Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+                Box::new(|cc| Box::new(web_rt::TemplateApp::new(cc))),
             )
             .await
             .expect("failed to start eframe");
